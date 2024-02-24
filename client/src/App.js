554172d099616df4fb58647887c06cfe426a1977
AdiJainy <adij1222@gmail.com>
2024-02-24 11:33:22 +0530

    initialised client and server folders

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-xl text-green-300">set up done</h1>
+    </div>
+  );
+}
+
+export default App;
