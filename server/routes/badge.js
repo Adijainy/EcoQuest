eb61693b6c0178b68bd27c64a47cf73c3899a1dc
AdiJainy <adij1222@gmail.com>
2024-02-25 14:52:46 +0530

    remove authentication for fetching the badges

--- server/routes/badge.js
+++ server/routes/badge.js
@@ -1,18 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 //importing badge controller
-const {createBadge, getAllBadges, allotBadge, getUserBadges} = require('../controllers/badge');
+const {
+  createBadge,
+  getAllBadges,
+  allotBadge,
+  getUserBadges,
+} = require("../controllers/badge");
 
 //importing auth middleware
-const {auth, isAdmin} = require('../middlewares/auth');
+const { auth, isAdmin } = require("../middlewares/auth");
 
 //badge routes
-router.post('/createBadge', auth, isAdmin, createBadge);
-router.get('/getAllBadges', auth, getAllBadges);
-router.post('/allotBadge', auth, allotBadge);
-router.get('/getUserBadges', auth, getUserBadges);
-
+router.post("/createBadge", auth, isAdmin, createBadge);
+router.get("/getAllBadges", getAllBadges);
+router.post("/allotBadge", auth, allotBadge);
+router.get("/getUserBadges", auth, getUserBadges);
 
 module.exports = router;
